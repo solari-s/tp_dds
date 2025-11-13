933138dfdd18a774a10e8d9777bca95c043bec3d
Barto <solaribarto@gmail.com>
2025-11-13 16:08:13 -0300

    Movi el js y el css, segui trabajando en main

--- entorno/src/main/java/com/app/main/Main.java
+++ entorno/src/main/java/com/app/main/Main.java
@@ -1,13 +1,19 @@
 package com.app.main;
 
-import java.util.Scanner;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.Scanner;
 import com.app.gestores.GestorHuesped;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.app.main", "com.app.gestores", "com.app.huesped", "com.app.responsablePago" })
+@EnableJpaRepositories(basePackages = { "com.app.huesped", "com.app.responsablePago" })
+@EntityScan(basePackages = { "com.app.huesped", "com.app.responsablePago" })
+
 public class Main {
 
     public static void main(String[] args) {
